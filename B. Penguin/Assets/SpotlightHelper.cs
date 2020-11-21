beae5cb67b15f4eb52399b217eeab3d09aa9fb34
kornfan774 <ironcladalt@gmail.com>
2020-11-21 05:21:12 -0600

    nes cartridge

--- B. Penguin/Assets/SpotlightHelper.cs
+++ B. Penguin/Assets/SpotlightHelper.cs
@@ -4,62 +4,38 @@ using UnityEngine;
 
 public class SpotlightHelper : MonoBehaviour
 {
-    public Vector3 FocalPoint;
+    //public Vector3 FocalPoint;
     public GameObject Spotlight;
-
-    //public float Timer = 0f;
-    public float DisableTimer = 15f;
-    public float DisableRecoveryTimer = 2f;
-    public float RotateTimer = 5f;
-    public float RotateRecoveryTimer = 2f;
-    public float MoveTimer = 10f;
-
-    private float CachedDisableTimer;
-    private float CachedDisableRecoveryTimer;
-    private float CachedRotateTimer;
-    private float CachedRotateRecoveryTimer;
-    //private float CachedTimer;
-
-    public Vector3 rotation = Vector3.zero;    
+    public float LightOnTime = 10.0f;
+    public float RecoveryTime = 2.0f;
+    public bool spotlightIsActive = false;
 
     void Start()
     {
-        CachedDisableTimer = DisableTimer;
-        CachedDisableRecoveryTimer = DisableRecoveryTimer;
-        CachedRotateTimer = RotateTimer;
-        //CachedTimer = Timer;
+        StartCoroutine(LightSwitch(LightOnTime, RecoveryTime));
     }
 
-    void Update()
+    void LightsOff()
     {
-        //Timer += Time.deltaTime;
-        DisableTimer -= Time.deltaTime;
-        RotateTimer -= Time.deltaTime;
-        MoveTimer -= Time.deltaTime;
+        StopCoroutine("LightSwitch");
+        spotlightIsActive = false;
+        Spotlight.SetActive(false);
+    }
 
-        if (DisableTimer == 0)
+    IEnumerator LightSwitch(float onTime, float waitTime )
+    {
+        spotlightIsActive = true;
+        while(true)
         {
+            Spotlight.SetActive(true);
+            yield return new WaitForSeconds(onTime);
             Spotlight.SetActive(false);
-            DisableRecoveryTimer -= Time.deltaTime;
-            
-            if(DisableRecoveryTimer == 0)
-            {
-                Spotlight.SetActive(true);
-                DisableRecoveryTimer = CachedDisableRecoveryTimer;
-                DisableTimer = CachedDisableTimer;
-            }
-        }
-
-        if (RotateTimer == 0)
-        {
-            transform.Rotate(rotation * Time.deltaTime);
-            RotateRecoveryTimer -= Time.deltaTime;
-
-            if (RotateRecoveryTimer == 0)
-            {
-                //stop rotating, reset
-                RotateRecoveryTimer = CachedRotateRecoveryTimer;
-            }
+            yield return new WaitForSeconds(waitTime);
         }
     }
+
+    /*IEnumerator RotateLights()
+    {
+
+    }*/
 }
